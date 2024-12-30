5c6cff7cad2e63c377deb199303572dfa7f6e757
Wanderjump <144531154+Wanderjump@users.noreply.github.com>
2024-12-30 13:12:03 +0300

    30.12.2025 - 01

--- Приложение 1. Извлечение данных внешних факторов о погоде.py
+++ Приложение 1. Извлечение данных внешних факторов о погоде.py
@@ -1,7 +1,7 @@
 import requests
 import pandas as pd
 
-# Замените 'your_api_key', 'your_city' на ваш ключ API от OpenWeatherMap и необходимый город
+# Заменим 'your_api_key', 'your_city' на полученный ключ API от OpenWeatherMap и необходимый город
 API_KEY = 'your_api_key'
 CITY = 'your_city'
 URL = f'http://api.openweathermap.org/data/2.5/weather?q={CITY}&appid={API_KEY}&units=metric'
